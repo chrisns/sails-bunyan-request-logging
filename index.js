db8b90b213da83a383275740e6011e933661d737
Robin Knipe <robin.knipe@gmail.com>
2016-03-23 15:34:42 +0000

    Created a sails `hook` to log deatils about all incoming requests.

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by robin on 23/03/16.
+ */
+
+const bunyan = require('bunyan');
+const log = bunyan.createLogger({name: "removals_integration"});
+
+module.exports = function (sails) {
+  return {
+    routes: {
+      after: {
+        '*': function (req, res, next) {
+          const headers = req.isSocket ? req.socket.handshake.headers : req.headers;
+          log.info({
+            method: req.method,
+            hostip: headers['x-real-ip'] || req.ip || req.socket.handshake.address,
+            path: req.url,
+            user: headers['x-auth-userid'],
+            status: res.statusCode
+          }, 'request');
+          return next();
+        }
+      }
+    }
+  };
+};
